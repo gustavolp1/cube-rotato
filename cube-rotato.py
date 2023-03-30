4b91dbb3b57fe90587121de77815048c5724743d
gustavolp1 <glpachecocsa@gmail.com>
2023-03-30 11:03:31 -0300

    cube wip

--- cube-rotato.py
+++ cube-rotato.py
@@ -1,6 +1,50 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import cv2 as cv
-import pandas as pd
 import pygame
+import math
+
+# Colors
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+
+# Aspect Ratio: 16:9
+width = 1280
+height = 720
+
+# Screen Setup
+pygame.display.set_caption('Cube Rotato')
+screen = pygame.display.set_mode((width, height))
+
+# Cube setup using NumPy
+
+scale = 100
+circle_pos = [width/2, height/2]
+
+points = []
+for x in (-1, 1):
+    for y in (-1, 1):
+        for z in (-1, 1):
+            points.append(np.array([x, y, z]))
+
+projection_matrix = np.array([ [1,0,0],[0,1,0],[0,0,0] ])
+
+# Base PyGame loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+
+    screen.fill(BLACK)
+    
+    for point in points:
+        projected2d = np.dot(projection_matrix, point.reshape((3,1)))
+        x = int(projected2d[0][0] * scale) + circle_pos[0]
+        y = int(projected2d[1][0] * scale) + circle_pos[1]
+        pygame.draw.circle(screen, GREEN, (x, y), 5)
+
+    pygame.display.update()
